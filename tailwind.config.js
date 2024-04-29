48e7effa1850864169d4e71eb0d32b2791f94196
Devara Dinata <dinatadevara@gmail.com>
2024-04-29 11:49:08 +0800

    tes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '128': '32rem',
+        '160': '40rem'
+      },
+      colors: {
+        'black-bg': '#1E1E1E',
+        'button-bg': '#F6995C',  
+        'warna-tempat': '#7A8089',
+      }
+    },
   },
   plugins: [],
 }
