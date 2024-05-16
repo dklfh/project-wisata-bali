f2afa549165432d5dc3b62515ef5413f8c92b752
AgungMantra <agung.mantra07@gmail.com>
2024-05-16 12:40:00 +0800

    test

---
+++ src/component/filteredbutton.jsx
@@ -0,0 +1,16 @@
+function filteredButon() {
+  return (
+    <div className="flex  gap-3">
+      <button class="text-white px-4 md:px-5 py-2 md:py-3    bg-button-bg font-bold rounded-full">
+        Beach
+      </button>
+      <button class="text-white px-4 md:px-5 py-2 md:py-3 bg-button-bg font-bold rounded-full">
+        Mountain
+      </button>
+      <button class="text-white px-4 md:px-5 py-2 md:py-3 bg-button-bg font-bold rounded-full">
+        Club
+      </button>
+    </div>
+  );
+}
+export default filteredButon;
